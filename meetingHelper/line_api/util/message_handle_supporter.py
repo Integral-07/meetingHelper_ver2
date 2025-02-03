68c74bbd4c2ddbad572482cbade08bb51f102a3e
Integral <tk.07@outlook.jp>
2025-02-03 15:14:24 +0000

    A: views.py->message-event(欠席連絡フェーズ・欠席理由登録および更新フェーズ)

--- meetingHelper/line_api/util/message_handle_supporter.py
+++ meetingHelper/line_api/util/message_handle_supporter.py
@@ -18,3 +18,12 @@ def isNameFieldEmpty(_user_id):
         return True
     else:
         return False
+    
+def isAbsentFlag(_user_id):
+
+    member = Member.objects.get(user_id=_user_id)
+    
+    if member.absent_flag == 1:
+        return True
+    else:
+        return False
