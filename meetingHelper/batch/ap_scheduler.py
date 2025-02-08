10bb183ba1e4e09c2062bcadfa185ea8f703d946
Integral <tk.07@outlook.jp>
2025-02-08 08:54:59 +0000

    render

--- meetingHelper/batch/ap_scheduler.py
+++ meetingHelper/batch/ap_scheduler.py
@@ -18,14 +18,15 @@ def periodic_execution():
 
 
 def start():
-
+    """
   day_of_weeks = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
   schedule = System.objects.get(id=0).meeting_DayOfWeek
 
   schedule_dayafter_index = day_of_weeks.index(schedule) + 1
   if schedule_dayafter_index >= 7:
      schedule_dayafter_index = 0
+    """
 
-  scheduler = BackgroundScheduler()
-  scheduler.add_job(periodic_execution, 'cron', hour=23, day_of_week=day_of_weeks[schedule_dayafter_index])
-  scheduler.start()
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(periodic_execution, 'cron', hour=23)#, day_of_week=day_of_weeks[schedule_dayafter_index])
+    scheduler.start()
